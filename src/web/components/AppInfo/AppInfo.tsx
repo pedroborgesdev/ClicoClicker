d4e7aca8745871fb79aea58a67a67d69aaa1e44c
pedroborgesdev <pedrofilipe.github@gmail.com>
2025-10-08 22:32:16 -0300

    fix: versioning

--- src/web/components/AppInfo/AppInfo.tsx
+++ src/web/components/AppInfo/AppInfo.tsx
@@ -1,12 +1,15 @@
+import React from 'react';
+import packageJson from '../../../../package.json';
+
 const AppInfo: React.FC = () => {
-    return (
-        <div className="bg-black-900 h-8 flex justify-center items-center rounded-b-2xl">
-            <div className="text-center text-xs text-gray-400 flex flex-row justify-center items-center gap-x-4">
-                <p>Clico 1.0.0 BETA</p>
-                <p>Creator: Pedro Borges (pedroborgesdev)</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="bg-black-900 h-8 flex justify-center items-center rounded-b-2xl">
+      <div className="text-center text-xs text-gray-400 flex flex-row justify-center items-center gap-x-4">
+        <p>ClicoClicker v{packageJson.version} BETA</p>
+        <p>Creator: Pedro Borges (@pedroborgesdev)</p>
+      </div>
+    </div>
+  );
 };
 
 export default AppInfo;
